40d7a638bef0ff2170e939bf6c69887b66797fb8
Alien73 <71262677+Alien73@users.noreply.github.com>
2020-09-14 17:54:50 +0700

    Add files via upload

---
+++ lib/faster.py
@@ -0,0 +1,4 @@
+a = input("var : ")
+b = input("var : ")
+
+print("%s,%s,n1,%s,%s,n2,%s,%s,n3,%s,%s,n4,%s,%s,n5,"%(a,b,a,b,a,b,a,b,a,b,cd))
---
+++ pwgenz.py
@@ -0,0 +1,36 @@
+import os
+
+def start():
+    os.system("sudo apt install figlet")
+    os.system("clear")
+    os.system("figlet pawgen-z")
+    print("\n[ Z Z Z ]")
+
+def play():
+    try:
+        filename = input("\n>> ENTER FILE NAME : ")
+        file=open(""+filename+".txt",'w')
+        a = input("\n>> Word 1 : ")
+        b = input("\n>> Word 2 : ")
+        c = input("\n>> Word 3 : ")
+        d = input("\n>> Word 4 : ")
+        e = input("\n>> Word 5 : ")
+        n1 = input("\n>> Word/Num 1 : ")
+        n2 = input("\n>> Word/Num 2 : ")
+        n3 = input("\n>> Word/Num 3 : ")
+        n4 = input("\n>> Word/Num 4 : ")
+        n5 = input("\n>> Word/Num 5 : ")
+        file.write("%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s"%(a,n1,a,n2,a,n3,a,n4,a,n5,b,n1,b,n2,b,n3,b,n4,b,n5,c,n1,c,n2,c,n3,c,n4,c,n5,d,n1,d,n2,d,n3,d,n4,d,n5,e,n1,e,n2,e,n3,e,n4,e,n5,a,b,n1,a,b,n2,a,b,n3,a,b,n4,a,b,n5,b,a,n1,b,a,n2,b,a,n3,b,a,n4,b,a,n5,a,c,n1,a,c,n2,a,c,n3,a,c,n4,a,c,n5,c,a,n1,c,a,n2,c,a,n3,c,a,n4,c,a,n5,a,d,n1,a,d,n2,a,d,n3,a,d,n4,a,d,n5,d,a,n1,d,a,n2,d,a,n3,d,a,n4,d,a,n5,a,e,n1,a,e,n2,a,e,n3,a,e,n4,a,e,n5,e,a,n1,e,a,n2,e,a,n3,e,a,n4,e,a,n5,b,c,n1,b,c,n2,b,c,n3,b,c,n4,b,c,n5,c,b,n1,c,b,n2,c,b,n3,c,b,n4,c,b,n5,b,d,n1,b,d,n2,b,d,n3,b,d,n4,b,d,n5,d,b,n1,d,b,n2,d,b,n3,d,b,n4,d,b,n5,b,e,n1,b,e,n2,b,e,n3,b,e,n4,b,e,n5,e,b,n1,e,b,n2,e,b,n3,e,b,n4,e,b,n5,c,d,n1,c,d,n2,c,d,n3,c,d,n4,c,d,n5,d,c,n1,d,c,n2,d,c,n3,d,c,n4,d,c,n5,c,e,n1,c,e,n2,c,e,n3,c,e,n4,c,e,n5,e,c,n1,e,c,n2,e,c,n3,e,c,n4,e,c,n5,d,e,n1,d,e,n2,d,e,n3,d,e,n4,d,e,n5,e,d,n1,e,d,n2,e,d,n3,e,d,n4,e,d,n5,e,e,n1,e,e,n2,e,e,n3,e,e,n4,e,e,n5,a,b,c,n1,a,b,c,n2,a,b,c,n3,a,b,c,n4,a,b,c,n5,b,c,a,n1,b,c,a,n2,b,c,a,n3,b,c,a,n4,b,c,a,n5,c,b,a,n1,c,b,a,n2,c,b,a,n3,c,b,a,n4,c,b,a,n5,b,a,c,n1,b,a,c,n2,b,a,c,n3,b,a,c,n4,b,a,c,n5,a,c,b,n1,a,c,b,n2,a,c,b,n3,a,c,b,n4,a,c,b,n5,c,a,b,n1,c,a,b,n2,c,a,b,n3,c,a,b,n4,c,a,b,n5,a,c,d,n1,a,c,d,n2,a,c,d,n3,a,c,d,n4,a,c,d,n5,c,d,a,n1,c,d,a,n2,c,d,a,n3,c,d,a,n4,c,d,a,n5,d,a,c,n1,d,a,c,n2,d,a,c,n3,d,a,c,n4,d,a,c,n5,c,a,d,n1,c,a,d,n2,c,a,d,n3,c,a,d,n4,c,a,d,n5,d,c,a,n1,d,c,a,n2,d,c,a,n3,d,c,a,n4,d,c,a,n5,a,d,c,n1,a,d,c,n2,a,d,c,n3,a,d,c,n4,a,d,c,n5,a,d,e,n1,a,d,e,n2,a,d,e,n3,a,d,e,n4,a,d,e,n5,e,d,a,n1,e,d,a,n2,e,d,a,n3,e,d,a,n4,e,d,a,n5,d,a,e,n1,d,a,e,n2,d,a,e,n3,d,a,e,n4,d,a,e,n5,e,a,d,n1,e,a,d,n2,e,a,d,n3,e,a,d,n4,e,a,d,n5,a,e,d,n1,a,e,d,n2,a,e,d,n3,a,e,d,n4,a,e,d,n5,d,e,a,n1,d,e,a,n2,d,e,a,n3,d,e,a,n4,d,e,a,n5,b,c,d,n1,b,c,d,n2,b,c,d,n3,b,c,d,n4,b,c,d,n5,d,c,b,n1,d,c,b,n2,d,c,b,n3,d,c,b,n4,d,c,b,n5,c,d,b,n1,c,d,b,n2,c,d,b,n3,c,d,b,n4,c,d,b,n5,b,d,c,n1,b,d,c,n2,b,d,c,n3,b,d,c,n4,b,d,c,n5,d,b,c,n1,d,b,c,n2,d,b,c,n3,d,b,c,n4,d,b,c,n5,c,b,d,n1,c,b,d,n2,c,b,d,n3,c,b,d,n4,c,b,d,n5,b,d,e,n1,b,d,e,n2,b,d,e,n3,b,d,e,n4,b,d,e,n5,e,d,b,n1,e,d,b,n2,e,d,b,n3,e,d,b,n4,e,d,b,n5,d,e,b,n1,d,e,b,n2,d,e,b,n3,d,e,b,n4,d,e,b,n5,b,e,d,n1,b,e,d,n2,b,e,d,n3,b,e,d,n4,b,e,d,n5,e,b,d,n1,e,b,d,n2,e,b,d,n3,e,b,d,n4,e,b,d,n5,d,b,e,n1,d,b,e,n2,d,b,e,n3,d,b,e,n4,d,b,e,n5,c,d,e,n1,c,d,e,n2,c,d,e,n3,c,d,e,n4,c,d,e,n5,e,d,c,n1,e,d,c,n2,e,d,c,n3,e,d,c,n4,e,d,c,n5,d,e,c,n1,d,e,c,n2,d,e,c,n3,d,e,c,n4,d,e,c,n5,c,e,d,n1,c,e,d,n2,c,e,d,n3,c,e,d,n4,c,e,d,n5,e,c,d,n1,e,c,d,n2,e,c,d,n3,e,c,d,n4,e,c,d,n5,d,c,e,n1,d,c,e,n2,d,c,e,n3,d,c,e,n4,d,c,e,n5,a,a,n1,a,a,n2,a,a,n3,a,a,n4,a,a,n5,b,b,n1,b,b,n2,b,b,n3,b,b,n4,b,b,n5,c,c,n1,c,c,n2,c,c,n3,c,c,n4,c,c,n5,d,d,n1,d,d,n2,d,d,n3,d,d,n4,d,d,n5,e,e,n1,e,e,n2,e,e,n3,e,e,n4,e,e,n5,a,a,a,n1,a,a,a,n2,a,a,a,n3,a,a,a,n4,a,a,a,n5,b,b,b,n1,b,b,b,n2,b,b,b,n3,b,b,b,n4,b,b,b,n5,c,c,c,n1,c,c,c,n2,c,c,c,n3,c,c,c,n4,c,c,c,n5,d,d,d,n1,d,d,d,n2,d,d,d,n3,d,d,d,n4,d,d,d,n5,e,e,e,n1,e,e,e,n2,e,e,e,n3,e,e,e,n4,e,e,e,n5))
+        os.system("clear")
+        print("\n[!] SUCCESSFUL [!]")
+        print("\n>> Saved as "+filename+".txt")
+        file.close()
+
+    except IOError:
+        os.system("clear")
+        print("\n[?] ERROR [?]")
+        os.system("clear")
+        play()
+
+start()
+play()
